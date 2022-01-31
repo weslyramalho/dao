bc0b68814ac2daa32c772ac6120a06a716b241cb
Wesly <weslyramalho@yahoo.com.br>
2022-01-31 17:41:53 -0300

    Reusando a instanciaçãoReusando a instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,6 +12,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("====Teste 1: Seller findById ====");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
