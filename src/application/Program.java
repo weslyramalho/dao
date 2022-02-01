e3756a798ad76704fc4f1af752c1bdbdd68adb40
Wesly <weslyramalho@yahoo.com.br>
2022-02-01 17:11:48 -0300

    implementação findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,8 +25,16 @@ public class Program {
 		for(Seller obj : list) {
 		System.out.println(obj);
 		}
+		
+		System.out.println("====Teste 3: Seller findAll ====");
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+		System.out.println(obj);
 
 
+
+	}
 	}
 
 }
